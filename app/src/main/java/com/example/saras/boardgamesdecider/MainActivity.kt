49d399e983a4b832c9cfba1156955ab3b5b97b7c
unknown <sara@j-r.in>
2019-01-07 14:56:55 +0100

    add new repository

---
+++ app/src/main/java/com/example/saras/boardgamesdecider/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.saras.boardgamesdecider
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val gamesList = arrayListOf("Terraformacja Marsa", "Dixit", "Talizman: Magia i Miecz")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        selectedGameBtn.setOnClickListener {
+            val random = Random()
+            val selectedGame = random.nextInt(gamesList.count())
+            selectedGameTxt.text = gamesList[selectedGame]
+        }
+        addGameBtn.setOnClickListener {
+            if(addGameTxt.text.count() > 0) {
+                val newGame = addGameTxt.text.toString()
+                gamesList.add(newGame)
+                addGameTxt.text.clear()
+            }
+        }
+    }
+}
